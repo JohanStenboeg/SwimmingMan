cd0332fe9f22731b622b83dd724e1adbd9ad383d
Johan <Joha6163@edu.easj.dk>
2018-01-31 11:20:27 +0100

    Sidste nye version, Tilføjet båd og det virker kinda nu

--- src/main/java/com/almasb/fxglgames/drop/BasicGameApp/WallBuildingControl.java
+++ src/main/java/com/almasb/fxglgames/drop/BasicGameApp/WallBuildingControl.java
@@ -5,6 +5,8 @@ import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.app.FXGL;
 import com.almasb.fxgl.entity.Entities;
 import com.almasb.fxgl.entity.component.CollidableComponent;
+import com.almasb.fxgl.physics.BoundingShape;
+import com.almasb.fxgl.physics.HitBox;
 import javafx.scene.shape.Rectangle;
 
 public class WallBuildingControl extends Control {
@@ -21,8 +23,13 @@ public class WallBuildingControl extends Control {
 
     private Rectangle wallView(double width, double height){
         Rectangle wall = new Rectangle(width, height);
-        wall.setArcWidth(25);
-        wall.setArcHeight(25);
+        wall.setArcWidth(50);
+        wall.setArcHeight(50);
+        wall.fillProperty().bind(FXGL.getApp().getGameState().objectProperty("stageColor"));
+        return wall;
+    }
+    private Rectangle topWallView(double width, double height){
+        Rectangle wall = new Rectangle(width, height);
         wall.fillProperty().bind(FXGL.getApp().getGameState().objectProperty("stageColor"));
         return wall;
     }
@@ -30,24 +37,29 @@ public class WallBuildingControl extends Control {
 
     private void buildWall() {
         double height = FXGL.getApp().getHeight();
-        double distance = height / 2;
-
-        for (int i = 1; i <= 10; i++){
+        double distance = height / 4;
+        Entity topWall;
+        Entity bottomWall;
+        for (int i = 1; i <= 20; i++){
             double topHeight = Math.random() * (height - distance);
 
-            Entities.builder()
-                    .at(lastWall + i * 500, 0 -25)
-                    .type(EntityType.WALL)
-                    .viewFromNodeWithBBox(wallView(50, topHeight))
+            topWall = Entities.builder() //Top wall
+                    .at(lastWall + i * 300, -300)
+                    .type(EntityType.TOPWALL)
+                    .viewFromTexture("BoatImage.png")
+                    .bbox(new HitBox("BODY", BoundingShape.box(10, 306)))
+                    //.viewFromNodeWithBBox(topWallView(50, topHeight))
                     .with(new CollidableComponent(true))
                     .buildAndAttach();
-            Entities.builder()
-                    .at(lastWall + i * 500, 0 + topHeight + distance + 25)
-                    .type(EntityType.WALL)
+            bottomWall = Entities.builder() //Bottom wall
+                    .at(lastWall + i * 300, 0 + topHeight + distance + 50)
+                    .type(EntityType.BOTTOMWALL)
+                    .bbox(new HitBox("BODY", BoundingShape.box(100, 50)))
                     .viewFromNodeWithBBox(wallView(50, height - distance - topHeight))
-                    .with(new CollidableComponent(true))
+                    .with(new CollidableComponent(false))
                     .buildAndAttach();
-
+            topWall.setScaleX(0.25);
+            topWall.setScaleY(0.25);
         }
         lastWall += 10 * 500;
     }
